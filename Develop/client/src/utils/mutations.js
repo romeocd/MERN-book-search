cb96dd85b8c2b709faa4cee654524eb00337d5cc
romeo <rdumlao07@gmail.com>
2024-01-30 21:25:54 -0500

    create SAVE_BOOK and REMOVE_BOOK mutations

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -29,3 +29,33 @@ export const ADD_USER = gql`
         }
     }
 `;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: BookInput!) {
+        saveBook(bookData: $bookData) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            savedBooks {
+                bookId
+                title
+            }
+        }
+    }
+`;
