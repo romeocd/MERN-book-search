dc5a10d29c1c2f2089715628d3b048e996b9ec56
romeo <rdumlao07@gmail.com>
2024-01-31 14:42:03 -0500

    add expressmiddleware and /graphql

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
 const path = require('path');
 const db = require('./config/connection');
 
@@ -30,8 +32,13 @@ app.use(express.json());
 const startApolloServer = async () => {
   await server.start();
 
-  // Apply Apollo Server middleware
-  server.applyMiddleware({ app });
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
