9ada47be53e67fd14fc2255ff914998ffd3dc3fc
romeo <rdumlao07@gmail.com>
2024-01-31 13:57:29 -0500

    replace addUser() with ADD_USER mutation

--- Develop/client/src/components/SignupForm.jsx
+++ Develop/client/src/components/SignupForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+//import useMutation
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +13,16 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser] = useMutation(ADD_USER, {
+    onCompleted: data => {
+      Auth.login(data.addUser.token);
+    },
+    onError: (error) => {
+      console.error(error);
+      setShowAlert(true);
+    }
+  });
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,18 +40,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      await addUser({
+        variables: { ...userFormData }
+      });
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
 
     setUserFormData({
