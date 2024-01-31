0b8a1f6664dd90b80eaf2b7664263ccf2f13ed44
romeo <rdumlao07@gmail.com>
2024-01-31 14:37:13 -0500

    update typeDefs to include mutations from resolvers

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,45 +1,47 @@
 const typeDefs = `
-    type User {
-        _id: ID
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
-    type Book {
-        bookId: String
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
 
-    type Auth {
-        token: String
-        user: User
-    }
+  type Auth {
+    token: String
+    user: User
+  }
 
-    input BookInput {
-        bookId: String
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
+  input BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
 
-    type Query{
-        me: User    
-    }
+  type Query {
+    me: User
+    getSingleUser(id: ID, username: String): User
+  }
 
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(bookData: BookInput!): User
-        removeBook(bookId: String!): User
-    }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    createUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: String!): User
+    deleteBook(bookId: String!): User
+  }
 `;
 
-module.exports = typeDefs; 
+module.exports = typeDefs;
