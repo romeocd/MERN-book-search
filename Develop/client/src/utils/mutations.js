1f78a0a916356d7df997409bad0b095a3102d829
romeo <rdumlao07@gmail.com>
2024-01-30 21:22:37 -0500

    create ADD_USER mutation

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql `
+export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String! ) {
         login(email: $email, password: $password) {
             token
@@ -17,3 +17,15 @@ export const LOGIN_USER = gql `
     }
 `;
 
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
