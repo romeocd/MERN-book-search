d70092db3405bce18994d84c6bf97e9afab8e655
romeo <rdumlao07@gmail.com>
2024-01-30 20:21:34 -0500

    Use the authMiddleware to add user info to the context

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,30 +1,42 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-
-const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
+
+// Import your typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
+// Import the updated authMiddleware
+const { authMiddleware } = require('./middleware/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware
+  context: async ({ req }) => {
+    // Use the authMiddleware to add user info to the context
+    const user = await authMiddleware({ req });
+    return { user };
+  }
 });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Start Apollo Server
 const startApolloServer = async () => {
   await server.start();
-  server.applyMiddleware({ app, path: '/graphql', cors: true });
+
+  // Apply Apollo Server middleware
+  server.applyMiddleware({ app });
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
   }
 
@@ -36,4 +48,5 @@ const startApolloServer = async () => {
   });
 };
 
+// Call the function to start the server
 startApolloServer();
