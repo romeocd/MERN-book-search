1934911148ba03297a027f12de6e46df9ec314a7
romeo <rdumlao07@gmail.com>
2024-02-01 12:33:57 -0500

    addUser is added to Auth.login

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -13,6 +13,9 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+  //addUser mutation hook
+  const [addUser] = useMutation(ADD_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -28,16 +31,10 @@ const SignupForm = () => {
       event.stopPropagation();
     }
 
+    //addUser is added to Auth.login
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await addUser({ variables: { ...userFormData } });
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
