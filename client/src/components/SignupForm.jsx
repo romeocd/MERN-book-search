69de98f9182cf690ffcd07fb1513325d544009cb
romeo <rdumlao07@gmail.com>
2024-02-01 12:31:16 -0500

    import ADD_USER useMutation

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
+import { ADD_USER } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
 
 const SignupForm = () => {
   // set initial form state
